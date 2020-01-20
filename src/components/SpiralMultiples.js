de8910ef5e2a8b0727c4c2c65f60d506a8a2a9dc
drivinward <edoardo.guido.93@gmail.com>
2020-01-20 14:55:55 +0100

    Basic interaction

--- src/components/SpiralMultiples.js
+++ src/components/SpiralMultiples.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import * as d3 from 'd3'
 import dirtyDataset from '../data/top50.json'
 import { SpiralShell } from './SpiralShell'
+import { numberOfDistinctElements } from '../lib/helpers'
 
 function cleanData(dataset) {
   return dataset.map((datum, i) => {
@@ -26,6 +27,9 @@ export default function SpiralMultiples(props) {
     })
   }, [])
 
+  const [hoveredElement, setHoveredElement] = useState(-1)
+  const [hovering, setHovering] = useState(false)
+
   const dataset = cleanData(dirtyDataset)
 
   // rename desired dataset columns to chart-specific properties
@@ -38,7 +42,11 @@ export default function SpiralMultiples(props) {
   //   }
   // })
   const datasetColumnNames = ['Genre', 'BPM', 'Loudness', 'Song Length']
+  //
+  // convert a discrete scale in a continuous one
+  const differentGenres = numberOfDistinctElements(dataset, 'genre')
 
+  //
   // svg constants
   const [margin] = useState({
     top: 80,
@@ -50,23 +58,28 @@ export default function SpiralMultiples(props) {
   const [height] = useState(720)
   const [viewBox] = useState([0, 0, width, height])
 
+  //
   // graph constants
   // chart
   const spiralInternalRadius = 10
   const spiralMaxAngle = d3.max(dataset, d => d.songLength)
   const spiralLinesCount = 200
   const circleRadius = 3
+  //
   // texts
   const fontSize = 10
   const lineHeight = 1.2
   const maxStringLength = 36
+  //
   // bottom axis
   const axisBottomLabelsPadding = 12
+  //
   // legend
   const legendEntryWidth = 260
-
+  const legendRectSize = 12
+  //
   // scales
-  const colorScheme = d3.scaleOrdinal(d3.schemeCategory10)
+  const colorScheme = d3.scaleSequential(d3.interpolateSinebow).domain([0, differentGenres.length])
   const xScale = d3
     .scaleLinear()
     .range([0, width - margin.right])
@@ -76,71 +89,104 @@ export default function SpiralMultiples(props) {
     .range([height - margin.bottom, margin.top])
     .domain(d3.extent(dataset, d => d.bpm))
 
+  //
+  // trim string only if trimmed character is a space
   const trimLongString = (str, maxLength) => {
     // console.log(str.slice(maxLength))
     return str
   }
 
+  //
   // axes
   // const xAxis = d3.axisBottom(xScale)
   // const yAxis = d3.axisLeft(yScale)
 
+  //
+  // interaction
+  const handleMouseEnter = index => {
+    setHoveredElement(index)
+    setHovering(true)
+    console.log(index, hoveredElement)
+  }
+
+  const handleMouseOut = () => {
+    setHovering(false)
+  }
+
+  const lowerSpiral = (index, scaleCategory) => {
+    // hovering === true && index === hoveredElement ? yScale(scale) : height
+    if (hovering === true) {
+      if (index === hoveredElement) {
+        return yScale(scaleCategory)
+      } else return height
+    } else return yScale(scaleCategory)
+  }
+
   return (
     <div className="chart">
       <h2>Responsive spiral Chart with React and D3</h2>
-      {debug && <span className="debug">Debug Mode</span>}
       <svg width={width} height={height} viewBox={viewBox} preserveAspectRatio="xMidYMin meet">
         <g className="legend" transform={`translate(0, ${margin.top})`}>
           {datasetColumnNames.map((legendEntry, j) => {
             return (
               <g key={j} transform={`translate(${legendEntryWidth * j}, 0)`}>
-                <text>{legendEntry}</text>
+                {/* <rect
+                  width={legendRectSize}
+                  height={legendRectSize}
+                  x={0}
+                  y={-legendRectSize}
+                  color={colorScheme(legendEntry)}
+                >
+                  {legendEntry}
+                </rect> */}
+                <text x={legendRectSize * 1.5}>{legendEntry}</text>
               </g>
             )
           })}
         </g>
-        <g transform={`translate(${margin.left}, 0)`}>
-          {dataset.map((datum, i) => (
-            <>
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+          {dataset.map((datum, i) => {
+            const color = colorScheme(differentGenres.indexOf(datum.genre))
+            return (
               <g
-                transform={`translate(${xScale(i) - fontSize / 2}, ${height -
-                  margin.bottom / 2 +
-                  axisBottomLabelsPadding}) rotate(45)`}
-                fontSize={`${fontSize}px`}
+                key={i}
+                onMouseEnter={() => handleMouseEnter(i)}
+                onMouseOut={() => handleMouseOut()}
               >
-                <text y={fontSize * lineHeight * 0} fontWeight="500">
-                  {trimLongString(datum.track, maxStringLength)}
-                </text>
-                <text y={fontSize * lineHeight * 1}>
-                  {trimLongString(datum.artist, maxStringLength)}
-                </text>
+                <g transform={`translate(${xScale(i)}, ${lowerSpiral(i, datum.bpm)})`}>
+                  <circle opacity="1" fill={color} cx="0" cy="0" r={circleRadius} />
+                  <line
+                    stroke="black"
+                    opacity="0.25"
+                    x1="0"
+                    y1={height - yScale(datum.bpm) - margin.bottom / 2}
+                    x2="0"
+                    y2={spiralInternalRadius}
+                  />
+                  <SpiralShell
+                    debug={debug}
+                    linesCount={spiralLinesCount}
+                    color={color}
+                    angle={(datum.songLength / spiralMaxAngle) * (2 * Math.PI)}
+                    internalRadius={spiralInternalRadius}
+                  />
+                </g>
+                <g
+                  transform={`translate(${xScale(i) - fontSize / 2}, ${height -
+                    margin.bottom / 2 +
+                    axisBottomLabelsPadding}) rotate(45)`}
+                  fontSize={`${fontSize}px`}
+                >
+                  <text y={fontSize * lineHeight * 0} fontWeight="500">
+                    {trimLongString(datum.track, maxStringLength)}
+                  </text>
+                  <text y={fontSize * lineHeight * 1}>
+                    {trimLongString(datum.artist, maxStringLength)}
+                  </text>
+                </g>
               </g>
-              <g key={i} transform={`translate(${xScale(i)}, ${yScale(datum.bpm)})`}>
-                <circle
-                  opacity="1"
-                  fill={colorScheme(datum.genre)}
-                  cx="0"
-                  cy="0"
-                  r={circleRadius}
-                />
-                <line
-                  stroke="black"
-                  opacity="0.25"
-                  x1="0"
-                  y1={height - yScale(datum.bpm) - margin.bottom / 2}
-                  x2="0"
-                  y2={spiralInternalRadius}
-                />
-                <SpiralShell
-                  debug={debug}
-                  linesCount={spiralLinesCount}
-                  color={colorScheme(datum.genre)}
-                  angle={(datum.songLength / spiralMaxAngle) * (2 * Math.PI)}
-                  internalRadius={spiralInternalRadius}
-                />
-              </g>
-            </>
-          ))}
+            )
+          })}
         </g>
       </svg>
     </div>
