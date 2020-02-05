1d7110780117d426e67bfecda961129df1b09cc9
drivinward <edoardo.guido.93@gmail.com>
2020-02-05 10:31:47 +0100

    🔕 Remove unnecessary files for current branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import SpiralMultiplesCanvas from './SpiralMultiplesCanvas'
 import { Balloons } from './Balloons'
 
 export function App() {
   return (
     <>
       <Balloons />
-      {/* <SpiralMultiplesCanvas /> */}
     </>
   )
 }
